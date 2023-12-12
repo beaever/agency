a221297679f50b6414be266e7b5dc7e7acef5a55
beaever <thebeaever@gmail.com>
2023-12-12 21:53:48 +0900

    workspace linking

---
+++ packages/components/src/components/index.tsx
@@ -0,0 +1,7 @@
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Accordions } from './Accordions';
+export { default as Box } from './Box';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
