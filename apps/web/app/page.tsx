4ebeea6cce44bb4903e4ddfa868aff25faae2b4d
beaever <thebeaever@gmail.com>
2023-12-13 23:47:43 +0900

    pipeline setting

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { Card } from 'ui';
-// import Button from "components";
+// import {Button} from "components"
 import styles from './page.module.css';
 
 function Gradient({
