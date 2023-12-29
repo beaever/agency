a7398dace6890e2f4133da3c24fc24925c23fb9b
beaever <thebeaever@gmail.com>
2023-12-29 20:17:45 +0900

    theme 상수 정의

--- packages/ui-kit/src/styles/themes.tsx
+++ packages/ui-kit/src/styles/themes.tsx
@@ -1,3 +1,11 @@
+import {
+  Theme,
+  PaletteType,
+  InterationType,
+  ElementType,
+  FontsProps,
+} from '@emotion/react';
+
 interface ColorsProps {
   palette: {
     // Red
@@ -11,7 +19,6 @@ interface ColorsProps {
     red80: string;
     red90: string;
     red100: string;
-
     // Blue
     blue10: string;
     blue20: string;
@@ -106,160 +113,108 @@ interface ColorsProps {
   };
 }
 
-// TODO: 세운님 작업 예정 (세운님 위와 같은 형식으로 interface 작성해주시면 되고, 궁금하신거 있으시면 언제든 연락주세요!)
-// Typography
-interface FontsProps {
-  size: {
-    size40: string;
-    size35: string;
-    size26: string;
-    size18: string;
-    size16: string;
-    size14: string;
-    size12: string;
-  };
-  weight: {
-    weight100: string;
-    weight200: string;
-    weight300: string;
-    weight400: string;
-    weight500: string;
-    weight600: string;
-    weight700: string;
-    weight800: string;
-    weight900: string;
-  };
-  style: {
-    heading1: string;
-    heading2: string;
-    title1: string;
-    title2: string;
-    title3: string;
-    subtitle1: string;
-    subtitle2: string;
-    subtitle3: string;
-    body1: string;
-    body2: string;
-    body3: string;
-    body4: string;
-    body5: string;
-    body6: string;
-    detail1: string;
-    detail2: string;
-  };
-}
+const palette: PaletteType = {
+  // RED
+  red10: '#FFF7F7',
+  red20: '#FFE7E7',
+  red30: '#FFDFDF',
+  red40: '#FFBEBE',
+  red50: '#FF9E9E',
+  red60: '#FF7D7D',
+  red70: '#FF5D5D',
+  red80: '#ED1C24',
+  red90: '#C00707',
+  red100: '#A70006',
+  //BLUE
+  blue10: '#eaf3ff',
+  blue20: '#D9E9FF',
+  blue30: '#B9D5FF',
+  blue40: '#9CC2F8',
+  blue50: '#7FAFF5',
+  blue60: '#5394F4',
+  blue70: '#3381F2',
+  blue80: '#0A68EF',
+  blue90: '#0759D9',
+  blue100: '#004DBC',
+  // AMBER
+  amber10: '#FFF',
+  amber20: '#FEE7DA',
+  amber30: '#FEC7A8',
+  amber40: '#FDA675',
+  amber50: '#FC8643',
+  amber60: '#FC762A',
+  amber70: '#F05903',
+  amber80: '#BD4603',
+  amber90: '#8B3402',
+  amber100: '#401801',
+  // GRAYCALE
+  gray10: '#FFF',
+  gray20: '#FCFCFC',
+  gray30: '#F7F7F7',
+  gray40: '#EFEFEF',
+  gray50: '#E6E6E6',
+  gray60: '#ABABAB',
+  gray70: '#727272',
+  gray80: '#4A4A4A',
+  gray90: '#2B2B2B',
+  gray100: '#1A1A1A',
+  // transparent
+  transparent: 'transparent',
+};
 
-const Colors: ColorsProps = {
-  // 기본
-  palette: {
-    // RED
-    red10: '#FFF7F7',
-    red20: '#FFE7E7',
-    red30: '#FFDFDF',
-    red40: '#FFBEBE',
-    red50: '#FF9E9E',
-    red60: '#FF7D7D',
-    red70: '#FF5D5D',
-    red80: '#ED1C24',
-    red90: '#C00707',
-    red100: '#A70006',
+const element: ElementType = {
+  // Primary
+  primary50: '#FFF',
+  primary100: '#FEE7DA',
+  primary200: '#FEC7A8',
+  primary300: '#FDA675',
+  primary400: '#FC8643',
+  primary500: '#FC762A',
+  primary600: '#F05903',
+  primary700: '#BD4603',
+  primary800: '#8B3402',
+  primary900: '#401801',
 
-    //BLUE
-    blue10: '#eaf3ff',
-    blue20: '#D9E9FF',
-    blue30: '#B9D5FF',
-    blue40: '#9CC2F8',
-    blue50: '#7FAFF5',
-    blue60: '#5394F4',
-    blue70: '#3381F2',
-    blue80: '#0A68EF',
-    blue90: '#0759D9',
-    blue100: '#004DBC',
+  // Gray Scale
+  grayScale50: '#FFF',
+  grayScale100: '#FCFCFC',
+  grayScale200: '#F7F7F7',
+  grayScale300: '#EFEFEF',
+  grayScale400: '#E6E6E6',
+  grayScale500: '#ABABAB',
+  grayScale600: '#727272',
+  grayScale700: '#4A4A4A',
+  grayScale800: '#2B2B2B',
+  grayScale900: '#1A1A1A',
 
-    // AMBER
-    amber10: '#FFF',
-    amber20: '#FEE7DA',
-    amber30: '#FEC7A8',
-    amber40: '#FDA675',
-    amber50: '#FC8643',
-    amber60: '#FC762A',
-    amber70: '#F05903',
-    amber80: '#BD4603',
-    amber90: '#8B3402',
-    amber100: '#401801',
+  // ALERT
+  warm100: '#FFE2E2',
+  warm500: '#FF6060',
+  suc100: '#C3D7FF',
+  suc500: '#4850FF',
+};
 
-    // GRAYCALE
-    gray10: '#FFF',
-    gray20: '#FCFCFC',
-    gray30: '#F7F7F7',
-    gray40: '#EFEFEF',
-    gray50: '#E6E6E6',
-    gray60: '#ABABAB',
-    gray70: '#727272',
-    gray80: '#4A4A4A',
-    gray90: '#2B2B2B',
-    gray100: '#1A1A1A',
-
-    // Border
-    basicBorder: '#000',
-    focusBorder: '#fff',
-    transparentBorder: 'transparent',
-
-    //disabled
-    disabled: '#4A4A4A',
-
-    // transparent
-    transparent: 'transparent',
+const interaction: InterationType = {
+  base: {
+    focus: '#C3D7FF',
+    select: '#4850FF',
+    disabled: '#727272',
+    checked: '#004DBC',
   },
-  element: {
-    // Primary
-    primary50: '#FFF',
-    primary100: '#FEE7DA',
-    primary200: '#FEC7A8',
-    primary300: '#FDA675',
-    primary400: '#FC8643',
-    primary500: '#FC762A',
-    primary600: '#F05903',
-    primary700: '#BD4603',
-    primary800: '#8B3402',
-    primary900: '#401801',
-
-    // Gray Scale
-    grayScale50: '#FFF',
-    grayScale100: '#FCFCFC',
-    grayScale200: '#F7F7F7',
-    grayScale300: '#EFEFEF',
-    grayScale400: '#E6E6E6',
-    grayScale500: '#ABABAB',
-    grayScale600: '#727272',
-    grayScale700: '#4A4A4A',
-    grayScale800: '#2B2B2B',
-    grayScale900: '#1A1A1A',
-
-    // ALERT
-    warm100: '#FFE2E2',
-    warm500: '#FF6060',
-    suc100: '#C3D7FF',
-    suc500: '#4850FF',
-
-    // opacity
-    opacity: {
-      // white
-      white30: 'background-color: #fff; opacity: 30%;',
-      white50: 'background-color: #fff; opacity: 50%;',
-      white70: 'background-color: #fff; opacity: 70%;',
-
-      // black
-      black30: 'background-color: #1A1A1A; opacity: 30%;',
-      black50: 'background-color: #1A1A1A; opacity: 50%;',
-      black70: 'background-color: #1A1A1A; opacity: 70%;',
-    },
+  opacity: {
+    // white
+    white30: 'background-color: #fff; opacity: 30%;',
+    white50: 'background-color: #fff; opacity: 50%;',
+    white70: 'background-color: #fff; opacity: 70%;',
+    // black
+    black30: 'background-color: #1A1A1A; opacity: 30%;',
+    black50: 'background-color: #1A1A1A; opacity: 50%;',
+    black70: 'background-color: #1A1A1A; opacity: 70%;',
   },
 };
 
-const Fonts: FontsProps = {
+const fonts: FontsProps = {
   // font-size
-  // [size숫자] 보다 small, medium, large, 이런식으로 구분하는게 나을거 같기도 함
   size: {
     size40: 'font-size: 40px;',
     size35: 'font-size: 52px;',
@@ -302,4 +257,15 @@ const Fonts: FontsProps = {
   },
 };
 
-export { Colors, Fonts };
+const theme: Theme = {
+  palette,
+  element,
+  interaction,
+  font: {
+    size: fonts.size,
+    style: fonts.style,
+    weight: fonts.weight,
+  },
+};
+
+export { palette, element, interaction, fonts, theme };
